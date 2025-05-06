42432bcba3a4182ec6a5592884eeef2781888cc5
Trần Lê Công Ti <congti1812@gmail.com>
2025-05-06 23:14:37 +0700

    ti upcode

--- .history/backend/config_20250506230244.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    # Cấu hình kết nối MySQL
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql+mysqlconnector://root:123456@localhost:/photobooth')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'hahahahahaha'
--- .history/backend/config_20250506230541.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    # Cấu hình kết nối MySQL
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql+mysqlconnector://root:123456@localhost:3307/photobooth')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'hahahahahaha'
